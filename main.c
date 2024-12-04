145af8edf9afa387ca445e371456351daec93a5c
DOAjayi <125410155+FlashCCXII@users.noreply.github.com>
2024-12-04 00:36:59 -0600

    Update main.c

--- main.c
+++ main.c
@@ -3,32 +3,36 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include "alias_handler.c"
+#include <fcntl.h>
+#include <errno.h>
+#include <signal.h>
+#include "alias_handler.h"
+#include "command_handler.h"
+#include "shared_data.h" 
 
-
+// Constants
 #define MAX_LINE 512
 #define HISTORY_SIZE 20
 
-// Function Prototypes
-void interactive_mode();
-void batch_mode(const char *filename);
-void execute_command(char *command);
-void handle_cd(char *path);
-void handle_exit();
-void handle_path(char *args);
-void handle_myhistory(char *args);
-void add_cmd(char *cmd);
-void print_history();
-void execute_history(int num);
-
+// Global Variables
 char *history[HISTORY_SIZE];
 int history_count = 0;
 char *path = NULL;
 
+// Function Prototypes
+void interactive_mode();
+void batch_mode(const char *filename);
+void add_cmd(char *cmd);
+void print_history();
+void execute_history(int num);
+void handle_path(char *args);
+void handle_exit();
+
 int main(int argc, char *argv[]) {
     signal(SIGINT, SIG_IGN);   // Ignore Ctrl-C
     signal(SIGTSTP, SIG_IGN);  // Ignore Ctrl-Z
-// Initialize the path variable with the current PATH
+
+    // Initialize the path variable with the current PATH
     path = strdup(getenv("PATH"));
     if (!path) {
         perror("strdup");
@@ -38,16 +42,13 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Usage: %s [batchFile]\n", argv[0]);
         exit(1);
     }
-
     if (argc == 2) {
         batch_mode(argv[1]);
     } else {
         interactive_mode();
-    return 0;
     }
-
-
-       
+    return 0;
+}
 
 // Interactive Mode
 void interactive_mode() {
@@ -63,7 +64,8 @@ void interactive_mode() {
         line[strcspn(line, "\n")] = 0;
         if (strlen(line) == 0) continue;
 
-        execute_command(line);
+        add_cmd(line); // Add to history
+        handle_input(line); // Handle the command
     }
 }
 
@@ -77,168 +79,76 @@ void batch_mode(const char *filename) {
 
     char line[MAX_LINE];
     while (fgets(line, sizeof(line), file)) {
-        // Remove newline and ignore empty lines
-        line[strcspn(line, "\n")] = 0;
+        line[strcspn(line, "\n")] = 0; // Remove newline
         if (strlen(line) == 0) continue;
 
+        add_cmd(line); // Add to history
         printf("%s\n", line); // Echo the command
-        execute_command(line);
+        handle_input(line); // Handle the command
     }
 
     fclose(file);
 }
 
-// Execute Command
-void execute_command(char *command) {
-    char *args[MAX_LINE / 2 + 1];
-    char *token = strtok(command, " ");
-    int i = 0;
-
-    int input_redirect = -1; // File descriptor for input redirection
-    int output_redirect = -1; // File descriptor for output redirection
-
-    while (token && i < MAX_LINE / 2) {
-        if (strcmp(token, "<") == 0) { // Input redirection
-            token = strtok(NULL, " ");
-            input_redirect = open(token, O_RDONLY);
-            if (input_redirect < 0) {
-                perror("open for input");
-                return;
-            }
-        } else if (strcmp(token, ">") == 0) { // Output redirection
-            token = strtok(NULL, " ");
-            output_redirect = open(token, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-            if (output_redirect < 0) {
-                perror("open for output");
-                return;
-            }
-        } else {
-            args[i++] = token;
-        }
-        token = strtok(NULL, " ");
-    }
-    args[i] = NULL;
-
-    if (args[0] == NULL) return; // Ignore empty commands
-
-    // Check for built-in commands
-    if (strcmp(args[0], "cd") == 0) {
-        handle_cd(args[1]);
-    } else if (strcmp(args[0], "exit") == 0) {
-        handle_exit();
-    } else {
-        // Non-built-in commands: Use fork and exec
-        pid_t pid = fork();
-        if (pid < 0) {
-            perror("fork");
-        } else if (pid == 0) {
-            // Child process
-            setpgid(0, 0); // Create a new process group for the child
-            if (input_redirect != -1) {
-                dup2(input_redirect, STDIN_FILENO);
-                close(input_redirect);
-            }
-            if (output_redirect != -1) {
-                dup2(output_redirect, STDOUT_FILENO);
-                close(output_redirect);
-            }
-
-            if (execvp(args[0], args) < 0) {
-                perror("execvp");
-                exit(1);
-            }
-        } else {
-            // Parent process
-            setpgid(pid, pid);              // Set process group for the child
-            tcsetpgrp(STDIN_FILENO, pid);   // Give terminal control to child
-            waitpid(pid, NULL, WUNTRACED);  // Wait for the child process
-            tcsetpgrp(STDIN_FILENO, getpid()); // Restore terminal to parent
-        }
-    }
-
-    // Close file descriptors if opened
-    if (input_redirect != -1) close(input_redirect);
-    if (output_redirect != -1) close(output_redirect);
-}
-// Built-in Command: path
 void handle_path(char *args) {
     if (args == NULL) {
         // Print current PATH
         printf("Current PATH: %s\n", path);
-    } else {
-        // Add a directory to PATH if it's not already there
-        if (strstr(path, args)) {
-            printf("Directory '%s' is already in PATH.\n", args);
+    } else if (args[0] == '+') {
+        // Add to PATH
+        if (strstr(path, args + 1)) {
+            printf("Directory '%s' is already in PATH.\n", args + 1);
             return;
         }
-
-        // Add the new directory to PATH
+        // Append the new directory to PATH
         char *new_path = (char *)malloc(strlen(path) + strlen(args) + 2);
         if (!new_path) {
             perror("malloc");
             return;
         }
-
-        // Append the new directory to the existing PATH
-        if (strlen(path) > 0) {
-            sprintf(new_path, "%s:%s", path, args);
-        } else {
-            strcpy(new_path, args);
-        }
-
+        sprintf(new_path, "%s:%s", path, args + 1);
         free(path);
         path = new_path;
         printf("Updated PATH: %s\n", path);
-    }
-}
-// Built-in Command: cd
-void handle_cd(char *path) {
-    if (path == NULL) {
-        // If no path is specified, change to HOME directory
-        path = getenv("HOME");
-        if (path == NULL) {
-            fprintf(stderr, "cd: HOME not set\n");
-            return;
-        }
-    }
-    if (chdir(path) != 0) {
-        // Handle errors from chdir
-        fprintf(stderr, "cd: %s: %s\n", path, strerror(errno));
+    } else if (args[0] == '-') {
+        // Remove from PATH (implement accordingly)
+    } else {
+        fprintf(stderr, "Invalid argument for path command.\n");
     }
 }
 
-// Built-in Command: exit
-void handle_exit() {
-    printf("Exiting shell...\n");
-    free(path);
-    exit(0);
-}
 
-
-// Built-in Command: myhistory
+// Add Command to History
 void add_cmd(char *cmd) {
-    // Add command to history
-if (history_count < HISTORY_SIZE) {
+    if (history_count < HISTORY_SIZE) {
         history[history_count++] = strdup(cmd);
     } else {
-        // Shift commands to make room for the new one
-        free(history[0]);
+        free(history[0]); // Free the oldest command
         for (int i = 1; i < HISTORY_SIZE; i++) {
             history[i - 1] = history[i];
         }
         history[HISTORY_SIZE - 1] = strdup(cmd);
     }
 }
+
+// Print Command History
 void print_history() {
     for (int i = 0; i < history_count; i++) {
         printf("%d: %s\n", i + 1, history[i]);
     }
 }
+
+// Execute Command from History
 void execute_history(int num) {
     if (num <= 0 || num > history_count) {
         fprintf(stderr, "Invalid history number\n");
         return;
     }
-    // Execute the command stored in history[num - 1]
-    execute_command(history[num - 1]);
+    handle_input(history[num - 1]); // Execute the command
+}
+
+void handle_exit() {
+    printf("Exiting shell...\n");
+    free(path);
+    exit(0);
 }
