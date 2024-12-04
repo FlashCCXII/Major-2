0e1213ba5e71d8abf009bbe156a00a7f48232875
DOAjayi <125410155+FlashCCXII@users.noreply.github.com>
2024-12-04 00:38:00 -0600

    Update alias_handler.c

--- alias_handler.c
+++ alias_handler.c
@@ -1,38 +1,29 @@
+#include "alias_handler.h"
+#include "command_handler.h"
+#include "shared_data.h"
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include "command_handler.c"
-
-#define MAX_LINE 1024
 
 void handle_input(char *input) {
-	if (strncmp(input, "alias ", 6) == 0) {
-	char *alias = strtok(input + 6, "=");
-	char *command = strtok(NULL, "\n");
-	if (alias && command) {
-	handle_alias(alias, command);
-	printf("Alias %s set to %s\n", alias, command);
-	} else {
-	printf("Invalid alias syntax. Use: alias <name>=<command>\n");
+    if (strncmp(input, "alias ", 6) == 0) {
+        char *alias = strtok(input + 6, "=");
+        char *command = strtok(NULL, "\n");
+        if (alias && command) {
+            handle_alias(alias, command);
+            printf("Alias '%s' set to '%s'\n", alias, command);
+        } else {
+            printf("Invalid alias syntax. Use: alias <name>=<command>\n");
+        }
+    } else if (strncmp(input, "history", 7) == 0) {
+        print_history();
+    } else if (strncmp(input, "!!", 2) == 0) {
+        if (history_count > 0) {
+            handle_input(history[history_count - 1]);
+        } else {
+            printf("No commands in history.\n");
+        }
+    } else {
+        add_cmd(input);  
+        execute_command(input);
 	}
-	} else {
-	execute_command(input);
-	}
-	}
-int main() {
-	char input[MAX_LINE];
-
-	while (1) {
-	printf("shell> ");
-	if (fgets(input, MAX_LINE, stdin) != NULL) {
-	input[strlen(input) - 1] = '\0';
-	if (strcmp(input, "exit") == 0) {
-	handle_exit();
-	} else {
-	handle_input(input);
-	}
-	} else {
-	printf("Error reading input\n");
-	}
-	}
-	}
+}
