c7d2aac39fc82ce22d5b132ce2049e83ae16711a
DOAjayi <125410155+FlashCCXII@users.noreply.github.com>
2024-12-04 16:55:27 -0600

    Update alias_handler.h

--- alias_handler.h
+++ alias_handler.h
@@ -1,6 +1,8 @@
 #ifndef ALIAS_HANDLER_H
 #define ALIAS_HANDLER_H
 
-void handle_input(char *input);
+void handle_alias(char *alias, char *command);
+void execute_alias(char *alias);
+int is_alias(char *cmd);
 
 #endif // ALIAS_HANDLER_H
