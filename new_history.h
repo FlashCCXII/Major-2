5712aa134c2271eec6ab402f660c77078981ceb9
DOAjayi <125410155+FlashCCXII@users.noreply.github.com>
2024-12-04 15:49:33 -0600

    Create new_history.h

---
+++ new_history.h
@@ -0,0 +1,10 @@
+#ifndef NEW_HISTORY_H
+#define NEW_HISTORY_H
+
+// Function Prototypes
+void add_cmd(const char *cmd);
+void print_history();
+void execute_history_command(int num);
+void clear_history();
+
+#endif // NEW_HISTORY_H
